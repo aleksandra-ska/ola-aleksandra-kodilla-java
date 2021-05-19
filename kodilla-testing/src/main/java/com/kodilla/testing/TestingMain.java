2eb4b75618874ca0de8ed3d18c05f10f846728a7
Sasza_ <aleksandrapradelska@gmail.com>
2021-05-19 19:50:53 +0200

    Calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main (String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        Calculator calculator = new Calculator();
+        int sum = calculator.add(8, 17);
+        int result = calculator.substract(28, 1);
+
+        if(sum < 100000) {
+            System.out.println(sum);
+        }else{
+            System.out.println("Error!");
+        }
+        if(result < 100000) {
+            System.out.println(result);
+        }else{
+            System.out.println("Error!");
+        }
     }
+
 }
+
+
