4410cb239e6f76a8c7035c4551368807250f6fe4
Sasza_ <aleksandrapradelska@gmail.com>
2021-05-20 06:33:52 +0200

    Calculator test 1.0

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -4,15 +4,15 @@ import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
     public static void main (String[] args) {
         Calculator calculator = new Calculator();
-        int sum = calculator.add(8, 17);
-        int result = calculator.substract(28, 1);
+        int sum = calculator.add(8, 2);
+        int result = calculator.substract(10, 1);
 
-        if(sum < 100000) {
+        if(sum == 10) {
             System.out.println(sum);
         }else{
             System.out.println("Error!");
         }
-        if(result < 100000) {
+        if(result == 7) {
             System.out.println(result);
         }else{
             System.out.println("Error!");
