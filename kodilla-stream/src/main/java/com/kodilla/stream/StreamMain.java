939022540e766989e26dfbb3a233b86e4adf7537
Sasza_ <aleksandrapradelska@gmail.com>
2021-06-04 08:16:26 +0200

    Task 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,8 +3,11 @@ package com.kodilla.stream;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import static java.time.temporal.ChronoUnit.YEARS;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -12,13 +15,13 @@ public class StreamMain {
 
         Map<Integer, ForumUser> theResultUsers = forum.getList().stream()
                 .filter(s -> s.getSex() == 'M')
-                .filter(s -> s.getBirthDate().getYear() < 2001)
+                .filter(s -> YEARS.between(s.getBirthDate(), LocalDate.now()) >= 20)
                 .filter(s -> s.getPostsCount() >= 1)
                 .collect(Collectors.toMap(ForumUser::getIdentificator, forumUser -> forumUser));
 
-        System.out.println("# elements: " + theResultUsers.size());             // [2]
+        System.out.println("# elements: " + theResultUsers.size());
         theResultUsers.entrySet().stream()
-                .map(entry -> entry.getKey() + ": " + entry.getValue())                   // [3]
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
                 .forEach(System.out::println);
     }
 }
